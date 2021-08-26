7564239e73622af85634ce537311bd448fdb1157
Davut Kember <davutkmbr@gmail.com>
2021-08-27 02:32:09 +0300

    Remove image

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -19,7 +19,7 @@
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
                         aria-label="Open user menu">
-                        <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
+                        <span class="avatar avatar-sm"></span>
                         <div class="d-none d-xl-block ps-2">
                             <div>Davut Kember</div>
                             <div class="mt-1 small text-muted">Sorumlu</div>
